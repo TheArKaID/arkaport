131c4f3a071c12789cab2948aa08570291ce6317
Arifia Kasastra R <arka.progammer@gmail.com>
2020-11-23 21:24:38 +0700

    Add judgement when there's no skill

--- resources/views/livewire/admin/expertise/index.blade.php
+++ resources/views/livewire/admin/expertise/index.blade.php
@@ -14,31 +14,48 @@
                 @if (session()->has('success'))
                     <div class="alert alert-success" role="alert">{{ session('success') }}</div>
                 @endif
-                <table class="table table-hover table-responsive">
-                    <thead>
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">Tipe</th>
-                            <th scope="col">Nama</th>
-                            <th scope="col">Detail</th>
-                            <th scope="col">#</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($expertises as $index => $expertise)
+                @if (count($expertises)===0)
+                    <div class="row">
+                        <div class="col-md-5">
+                            <img alt="#" src="{{ asset('adminator/assets/static/images/500.png') }}" />
+                        </div>
+                        <div class="col-md-7">
+                            <h1 class="mB-30 fw-900 lh-1 c-red-500" style="font-size: 60px;">No Skills ?</h1>
+                            <h3 class="mB-10 fsz-lg c-grey-900 tt-c">Maaf anda Noob</h3>
+                            <p class="mB-30 fsz-def c-grey-700">Silahkan Isi Skill Anda</p>
+                            <div>
+                                <a href="{{ route('admin.expertise.create')}}" class="btn btn-primary">Tambah</a>
+                            </div>
+                        </div>
+                    </div>
+                @else
+                    <table class="table table-hover table-responsive">
+                        <thead>
                             <tr>
-                                <th scope="row">{{$index}}</th>
-                                <td>{{ $expertise->typeExpertised() }}</td>
-                                <td>{{ $expertise['name'] }}</td>
-                                <td>{{ $expertise['detail'] }}</td>
-                                <td>
-                                    <a href="{{ route('admin.expertise.detail', $expertise['id']) }}" class="btn btn-warning">Edit</a>
-                                    <a href="#" wire:click="delete({{ $expertise['id'] }})" class="btn btn-danger">Hapus</a>
-                                </td>
+                                <th scope="col">#</th>
+                                <th scope="col">Tipe</th>
+                                <th scope="col">Nama</th>
+                                <th scope="col">Detail</th>
+                                <th scope="col">#</th>
                             </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                            @foreach ($expertises as $index => $expertise)
+                                <tr>
+                                    <th scope="row">{{$index}}</th>
+                                    <td>{{ $expertise->typeExpertised() }}</td>
+                                    <td>{{ $expertise['name'] }}</td>
+                                    <td>{{ $expertise['detail'] }}</td>
+                                    <td>
+                                        <a href="{{ route('admin.expertise.detail', $expertise['id']) }}" class="btn btn-warning">Edit</a>
+                                        <a href="#" wire:click="delete({{ $expertise['id'] }})" class="btn btn-danger">Hapus</a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                @endif
+                
             </div>
         </div>
     </div>
